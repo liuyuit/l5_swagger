d124d62308377e5cb6e6d164428bf3ccb8097a29
liuyu <liuyuit@aliyun.com>
2020-09-30 16:50:54 +0800

    swagger

---
+++ app/Swagger/swagger-v3.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/17
+ * Time: 17:56
+ */
+
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Swagger Petstore",
+ *         description="This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.",
+ *         termsOfService="http://swagger.io/terms/",
+ *         @OA\Contact(
+ *             email="apiteam@swagger.io"
+ *         ),
+ *         @OA\Contact(
+ *             email="liuy@ggxx.net",
+ *             name="Frank Liu",
+ *         ),
+ *         @OA\License(
+ *             name="Apache 2.0",
+ *             url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *         )
+ *     ),
+ *     @OA\Server(
+ *         description="OpenApi host",
+ *         url="http://swagger.local/api"
+ *     ),
+ *     @OA\ExternalDocumentation(
+ *         description="Find out more about Swagger",
+ *         url="http://swagger.io"
+ *     )
+ * )
+ */
